7766c5b2b67df313ca0045787482bf059a860356
for3st <forest86@mail.ru>
2017-11-15 22:18:51 +0200

    * Switch to streams using

--- middlewares/aggregation.js
+++ middlewares/aggregation.js
@@ -1,17 +1,35 @@
-const axios = require('axios');
+// const axios = require('axios');
+const http = require('http');
 
 module.exports = (req, res, next) => {
-  const host = `${req.protocol}://${req.get('host')}`;
+  const parts = req.get('host').split(':');
   const queue = [];
   const pathNameMap = {};
 
-  Object.keys(req.query).forEach(s => {
-    let path = '/' + req.query[s];
-    pathNameMap[path] = s;
-    queue.push(axios.get(host + path));
-  });
+  const options = {
+    hostname: parts[0],
+    port: parts[1],
+    headers: req.headers,
+    timeout: 2000
+  };
 
-  Promise.all(queue)
-    .then(results => res.json(Object.assign.apply({}, results.map(result => ({ [pathNameMap[result.request.path]]: result.data })))))
-    .catch(e => res.status(500).send(e.response.statusText + ' ' + e.config.url));
+  res.setHeader('Content-Type', 'application/json');
+  res.write('{');
+
+  const loadResource = (name, res, options) =>
+    new Promise((resolve, reject) => {
+      http.get(options, response => {
+        res.write(`"${name}":`);
+        response.on('data', chunk => res.write(chunk));
+        response.on('end', () => resolve());
+        response.on('error', e => reject(e));
+      });
+    })
+
+  const tasks = Object.keys(req.query).map(key => () => loadResource(key, res, Object.assign({}, options, {path: '/' + req.query[key]})));
+
+  tasks.reduce(
+    (promise, task) => promise.then(() => (tasks[tasks.length - 1] === task) ? task() : task().then(() => res.write(','))),
+    Promise.resolve()
+  ).then(() => { res.write('}'); res.send(); });
 };
